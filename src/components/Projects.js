0c618f3100c0a4583bb48abb9aef8511855a7a7b
Joshua Hutchinson <jthutch94@gmail.com>
2018-07-25 15:19:11 -0400

    small changes to the projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,16 +15,16 @@ export default function Projects(props) {
                     <a className="github-link" href="https://github.com/thinkful-ei19/Final-CS-ctrl-alt-elite-server">GitHub Server</a>
                 </li>
                 <li className="project">
-                <a className="project-title" href="https://learn-dothraki.netlify.com/"> Learn Dothraki</a>
-                    <p>Learn Dothraki is a spaced repetition application made for the sole purpose of learning the fictional language Dothraki from the popular HBO television series Game of Thrones. The application was built using React.js, Redux, Express, and Node.</p>
+                <a className="project-title" href="https://dothrak-tionary.netlify.com/"> Dothrak-tionary</a>
+                    <p>Dothrak-tionary is a spaced repetition application made for the sole purpose of learning the fictional language Dothraki from the popular HBO television series Game of Thrones. The application was built using React.js, Redux, Express, and Node.</p>
                     <img src="https://s8.postimg.cc/yvlkk5vyt/Dothraki-_Screenshot.png" height="50%" width="75%" alt="Learn Dothraki Screenshot"/>
                     <a className="github-link" href="https://github.com/thinkful-ei19/Josh-Ray-Spaced-Repitition-App-Client">GitHub Client</a>
                     <br />
                     <a className="github-link"  href="https://github.com/thinkful-ei19/Josh-Ray-Spaced-Repetition-App-Server">GitHub Server</a>
                 </li>
                 <li className="project">
-                    <a className="project-title" href="https://rpg-character-builder.netlify.com/"> RPG Character Builder</a>
-                    <p>The RPG Character Builder is a full stack web application designed to allow users to choose from a preset list of options in order to create custom characters. The application is built using React.js, Redux, Express, and Node.</p>
+                    <a className="project-title" href="https://character-builder.netlify.com/">Character Builder</a>
+                    <p>The Character Builder is a full stack web application designed to allow users to choose from a preset list of options in order to create custom characters. The application is built using React.js, Redux, Express, and Node.</p>
                     <img src="https://s8.postimg.cc/t7f9td72t/Screenshot_44.png" height="50%" width="75%" alt="Character Builder Screenshot"/>
                     <a className="github-link" href="https://github.com/JTHUTCH94/Character-Builder-Client">GitHub</a>
                 </li>
